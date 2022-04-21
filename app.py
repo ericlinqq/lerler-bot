795d04ce1cb38d610b66e3fadde30d092d5e1872
ericlin <ericlinswag.tem06@nctu.edu.tw>
2022-04-21 17:02:32 +0800

    app: add button template message for choosing area, category and price range

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
+from linebot.models import (
+    MessageEvent,
+    PostbackEvent,
+    TextSendMessage,
+    StickerSendMessage,
+    ImageSendMessage
+)
 
 import configparser
 
 from foodScraper import IFoodie
+
+from message import AreaMessage, CategoryMessage, PriceMessage
 
 app = Flask(__name__)
 
@@ -45,16 +53,35 @@ def handle_follow(event):
 # 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 美食爬蟲
-    if "美食" in event.message.text:
-        food = IFoodie(event.message.text[2:])
-        message = TextSendMessage(text=food.scrape())
+    # 美食「選擇地區」樣板類別訊息
+    if event.message.text == "美食":
+        message = AreaMessage().content()  
 
-    if "樂樂" in event.message.text:
+    if event.message.text == "樂樂":
         message = ImageSendMessage(
             original_content_url = 'https://i.imgur.com/SuatGGC.jpg',
             preview_image_url = 'https://i.imgur.com/SuatGGC.jpg'
         )
+    line_bot_api.reply_message(event.reply_token, message)
+
+# 處理美食回傳值事件
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data[:1] == "A":  # 如果回傳值為「選擇地區」
+        message = CategoryMessage(event.postback.data[2:]).content()
+    elif event.postback.data[:1] == "B":  # 如果回傳值為「選擇美食類別」
+        message = PriceMessage(event.postback.data[2:]).content()
+    elif event.postback.data[:1] == "C":  # 如果回傳值為「選擇消費金額」
+        result = event.postback.data[2:].split('&')
+
+        food = IFoodie(
+            result[0], # 地區 
+            result[1], # 美食類別
+            result[2]  # 消費金額
+        )
+
+        message = TextSendMessage(text=food.scrape())
+
     line_bot_api.reply_message(event.reply_token, message)
 
 # 處理貼圖訊息
