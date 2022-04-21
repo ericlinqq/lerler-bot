7da5a46cb0d3936d4d4a9772ba92ad444f44b5ab
ericlin <ericlinswag.tem06@nctu.edu.tw>
2022-04-21 17:32:48 +0800

    foodScraper: modify to allow category and price input

--- foodScraper.py
+++ foodScraper.py
@@ -5,8 +5,10 @@ import requests
 # 美食抽象類別
 class Food(ABC):
 
-    def __init__(self, area):
+    def __init__(self, area, category, price):
         self.area = area # 地區
+        self.category = category # 美食類別
+        self.price = price #消費金額
 
     @abstractmethod
     def scrape(self):
@@ -16,7 +18,12 @@ class Food(ABC):
 class IFoodie(Food):
     
     def scrape(self):
-        response = requests.get("https://ifoodie.tw/explore/" + self.area + "/list?sortby=popular&opening=true")
+        response = requests.get(
+            "https://ifoodie.tw/explore/" + self.area + 
+            "/list/" + self.category + 
+            "?priceLevel=" + self.price +
+            "&sortby=popular&opening=true")
+
         soup = BeautifulSoup(response.content, "html.parser")
 
         # 爬取前五筆餐廳卡片資料
@@ -26,16 +33,16 @@ class IFoodie(Food):
         content = ""
         for card in cards:
 
-            title = card.find(
+            title = card.find(  # 餐廳名稱
                 "a", {"class": "jsx-558691085 title-text"}).getText()
             
-            rating = card.find(
+            rating = card.find(  # 餐廳評價
                 "div", {"class": "jsx-1207467136 text"}).getText()
             
-            address = card.find(
+            address = card.find(  #餐廳地址
                 "div", {"class": "jsx-558691085 address-row"}).getText()
             
-            url = 'https://ifoodie.tw' + card.find(
+            url = 'https://ifoodie.tw' + card.find(  # 餐廳愛食記網址
                 "a", {"class": "jsx-558691085 title-text"})['href']
 
             content += f"{title} \n{rating}顆星 \n{address} \n{url} \n\n"
