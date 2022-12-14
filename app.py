7fd378b771d9ee1bb02a56ae0ba2b419511cd089
ericlinqq <fictionman66@icloud.com>
2022-12-14 13:48:03 +0800

    add hello

--- app.py
+++ app.py
@@ -61,6 +61,10 @@ def setPrice(type, event):
 cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣'\
                     ,'新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
 
+# domain root
+@app.route('/')
+def home():
+    return "Hello World!"
 
     
 # 監聽所有來自 /callback 的 Post Request
