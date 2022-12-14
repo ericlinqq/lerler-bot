e604a962eab43f9a6ce04b66c441e8415707969c
ericlinqq <fictionman66@icloud.com>
2022-12-14 14:28:52 +0800

    app.run

--- app.py
+++ app.py
@@ -227,5 +227,6 @@ def handle_sticker(event):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
